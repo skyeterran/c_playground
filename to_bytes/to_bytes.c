2a18098874062ed4bcf68bca15bd51fa14e19157
Skye Terran <skyeterran@gmail.com>
2025-01-11 00:34:31 -0800

    Update to_bytes.c

--- to_bytes/to_bytes.c
+++ to_bytes/to_bytes.c
@@ -2,12 +2,15 @@
 #include <string.h>
 
 int main() {
-    int x = 420;
-    unsigned char bytes[sizeof(x)];
-    memcpy(bytes, &x, sizeof(x));
+    for (int x = 0; x < 1024; x++) {
+        unsigned char bytes[sizeof(x)];
+        memcpy(bytes, &x, sizeof(x));
 
-    for (int i = 0; i < sizeof(x); i++) {
-        printf("%02x ", bytes[i]);
+        printf("%d -> ", x);
+        for (int i = 0; i < sizeof(x); i++) {
+            printf("%02x ", bytes[i]);
+        }
+        printf("\n");
     }
 
     return 0;
